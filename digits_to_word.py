9045f94945874c73c4159e2a0dc8680f4fdacd1c
mary <134166373+marykargozar@users.noreply.github.com>
2024-02-29 16:55:27 +0100

    convert digits to word.

---
+++ digits_to_word.py
@@ -0,0 +1,17 @@
+phone = input('insert your phone number: ')
+digits_mapping = {
+    '0' : 'zero',
+    "1" : "one",
+    '2': 'two',
+    '3':'three',
+    '4':'four',
+    '5':'five',
+    '6':'six',
+    '7': 'seven',
+    '8': 'eight',
+    '9':'nine'
+}
+output = ""
+for ch in phone :
+   output += digits_mapping[ch] + " "
+print(output) 
